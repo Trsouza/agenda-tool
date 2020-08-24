496624a4cce27bb895b52fa924426718da079620
Tathi <tathianesouza1@gmail.com>
2020-08-24 01:51:35 -0300

    Cria o header o footer o nav e a home

--- agenda-front/src/app/app-routing.module.ts
+++ agenda-front/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './templates/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: "", /* Nome da rota */
+  component: HomeComponent
+},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- agenda-front/src/app/app.module.ts
+++ agenda-front/src/app/app.module.ts
@@ -4,15 +4,34 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { HeaderComponent } from './templates/header/header.component';
+import { FooterComponent } from './templates/footer/footer.component';
+import { HomeComponent } from './templates/home/home.component'; 
+import { NavComponent } from './templates/nav/nav.component'; 
+
+import { MatCardModule } from '@angular/material/card'
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatSidenavModule,
+    MatListModule,
+
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
